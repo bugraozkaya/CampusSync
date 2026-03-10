3ddf8e53dbbd37b4feb56a88bfd8aef4282f74a4
bugraozkaya <ahmetbugraozkaya14@gmail.com>
2026-03-11 01:15:52 +0300

    Monorepo yapısına geçildi: backend ve mobile klasörleri ayrıldı

---
+++ mobile/app/src/main/java/com/bugra/campussync/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bugra.campussync
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bugra.campussync.ui.theme.CampusSyncTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CampusSyncTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CampusSyncTheme {
+        Greeting("Android")
+    }
+}
